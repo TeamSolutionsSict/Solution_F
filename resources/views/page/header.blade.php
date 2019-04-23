7cf29acff5673354e194c020d2ba9580a0347398
nlpnguyen <nlpnguyen.17it3@sict.udn.vn>
2019-04-23 19:14:25 +0700

    23/04/2019-Nguyên

--- resources/views/page/header.blade.php
+++ resources/views/page/header.blade.php
@@ -1,3 +1,4 @@
+@if(!Auth::check())
 <div class="login-panel">
     <section class="container">
         <div class="row">
@@ -6,28 +7,28 @@
                     <h2>Login</h2>
                     <div class="form-style form-style-3">
                         <form action="{{ route('post.Login') }}" method="POST">
-                                {{ csrf_field() }}
-                                @if(Session::has('flag'))
-                                    <div class="alert-message {{ Session::get('flag') }}">{{ Session::get('message') }}</div>
-                                @endif
-                                <div class="form-inputs clearfix">
-                                    <p class="login-text">
-                                        <input type="text" value="Username" name="username" onfocus="if (this.value == 'Username') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Username';}">
-                                        <i class="icon-user"></i>
-                                    </p>
-                                    <p class="login-password">
-                                        <input type="password" value="Password" name="password" onfocus="if (this.value == 'Password') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Password';}">
-                                        <i class="icon-lock"></i>
-                                        <a href="#">Forget</a>
-                                    </p>
-                                </div>
-                                <p class="form-submit login-submit">
-                                    <input type="submit" value="Log in" class="button color small login-submit submit">
+                            {{ csrf_field() }}
+                            @if(Session::has('flag'))
+                                <div class="alert-message {{ Session::get('flag') }}">{{ Session::get('message') }}</div>
+                            @endif
+                            <div class="form-inputs clearfix">
+                                <p class="login-text">
+                                    <input type="text" value="Username" name="username" onfocus="if (this.value == 'Username') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Username';}">
+                                    <i class="icon-user"></i>
+                                </p>
+                                <p class="login-password">
+                                    <input type="password" value="Password" name="password" onfocus="if (this.value == 'Password') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Password';}">
+                                    <i class="icon-lock"></i>
+                                    <a href="#">Forget</a>
+                                </p>
+                            </div>
+                            <p class="form-submit login-submit">
+                                <input type="submit" value="Log in" class="button color small login-submit submit">
                                 </p>
                                 <div class="rememberme">
                                     <label><input type="checkbox" checked="checked"> Remember Me</label>
                                 </div>
-                            </form>
+                        </form>
                     </div>
                 </div><!-- End page-content -->
             </div><!-- End col-md-6 -->
@@ -47,46 +48,48 @@
     <div class="form-style form-style-3">
         <form method="post" action="{{route('post.Register')}}" enctype="multipart/form-data" ONCHANGE="checkRegister">
             <input id="register" type="hidden" name="_token" value="{!! csrf_token() !!}">
-            <div class="form-inputs clearfix">
+            <div class="form-inputs clearfix ">
                 <input class="inavatar" type="file" id="av"  name="fileavatar" onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])"  >
-                <img src="{{asset('page/images/avatar/boy-512.png')}}" class="avatarlogin" id="blah"   alt="your image"  />
-                    {{--src="page/images/avatar/boy-512.png"name="avatar"
-                       onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])"--}}
+                <img src="page/images/avatar/boy-512.png" class="avatarlogin" id="blah"   alt="your image"  />
+                {{--src="page/images/avatar/boy-512.png"name="avatar"
+                   onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])"--}}
+                <div id="response"></div>
                 <p>
-                    <label class="required">Username<span>*</span></label>
-                    <input type="text" name="username" id="username" >
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('username') ? ' has-error' : '' }}">Username<span>*</span></label>
+                    <input type="text" name="username" id="username" ONCHANGE="checkRegister()">
+                    <span class="messages">{{ $errors->first('username') }}</span>
                 </p>
                 <p>
-                    <label class="required">First Name<span>*</span></label>
-                    <input type="text" name="firstname" id="firstname">
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('firstname') ? ' has-error' : '' }}">Firstname<span>*</span></label>
+                    <input type="text" name="firstname" id="firstname" onchange="checkRegister()">
+                    <span class="messages">{{ $errors->first('firstname') }}</span>
                 </p>
                 <p>
-                    <label class="required">Last Name<span>*</span></label>
-                    <input type="text" name="lastname" id="lastname">
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('lastname') ? ' has-error' : '' }}">lastname<span>*</span></label>
+                    <input type="text" name="lastname" id="lastname" ONCHANGE="checkRegister()">
+                    <span class="messages">{{ $errors->first('lastname') }}</span>
                 </p>
                 <p>
-                    <label class="required">E-Mail<span>*</span></label>
-                    <input type="email" name="email" id="email">
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('email') ? ' has-error' : '' }}">E-Mail<span>*</span></label>
+                    <input type="email" name="email" id="email" ONCHANGE="checkRegister()">
+                    <span class="messages">{{ $errors->first('email') }}</span>
                 </p>
                 <p>
-                    <label class="required">Phone<span>*</span></label>
-                    <input type="text" name="phone" id="phone">
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('phone') ? ' has-error' : '' }}">Phone<span>*</span></label>
+                    <input type="text" name="phone" id="phone" ONCHANGE="checkRegister()">
+                    <span class="messages">{{ $errors->first('phone') }}</span>
                 </p>
                 <p>
-                    <label class="required">Password<span>*</span></label>
-                    <input type="password" name="password" id="password">
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('password') ? ' has-error' : '' }}">Password<span>*</span></label>
+                    <input type="password" name="password" id="password" onchange="checkRegister()">
+                    <span class="messages">{{ $errors->first('password') }}</span>
                 </p>
                 <p>
-                    <label class="required">Confirm Password<span>*</span></label>
-                    <input type="password" name="confirmpassword" value="">
-                    <span class="messages"></span>
+                    <label class="required {{ $errors->has('confirmpassword') ? ' has-error' : '' }}">Confirm Password<span>*</span></label>
+                    <input type="password" id="confirmpassword" name="confirmpassword" value="" ONCHANGE="checkRegister()">
+                    <span class="messages">{{ $errors->first('confirmpassword') }}</span>
                 </p>
+
             </div>
             <p class="form-submit">
                 <input type="submit" value="Signup" class="button color small submit">
@@ -96,28 +99,29 @@
 </div><!-- End signup -->
 
 <div class="panel-pop" id="lost-password">
-    <h2>Lost Password<i class="icon-remove"></i></h2>
+    <h2>Forgot Password<i class="icon-remove"></i></h2>
     <div class="form-style form-style-3">
         <p>Lost your password? Please enter your username and email address. You will receive a link to create a new password via email.</p>
-        <form>
+        <form method="post" action="{{route('post.EmailResetPass')}}">
+            <input id="register" type="hidden" name="_token" value="{!! csrf_token() !!}">
             <div class="form-inputs clearfix">
                 <p>
                     <label class="required">Username<span>*</span></label>
-                    <input type="text">
+                    <input type="text" name="username" id="username">
                 </p>
                 <p>
                     <label class="required">E-Mail<span>*</span></label>
-                    <input type="email">
+                    <input type="email" name="email" id="email">
                 </p>
             </div>
             <p class="form-submit">
-                <input type="submit" value="Reset" class="button color small submit">
+                <input type="submit" value="Rest Password" class="button color small submit">
             </p>
         </form>
         <div class="clearfix"></div>
     </div>
 </div><!-- End lost-password -->
-@endif
+
 <div id="header-top">
     <section class="container clearfix">
         <nav class="header-top-nav">
@@ -127,7 +131,7 @@
                 {{-- Sau khi đăng nhập, nút này sẽ thành Profile và ẩn đi form login --}}
                 @if(Auth::check())
                     <li><a href="{{ route('get.Profile', Auth::user()->id) }}"><i class="icon-user"></i>Profile</a></li>
-                @else             
+                @else
                     <li><a href="" id="login-panel"><i class="icon-user"></i>Login Area</a></li>
                 @endif
             </ul>
@@ -153,32 +157,68 @@
     </div>
 </header>
 <script >
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            console.log('ditm me');
-            reader.onload = function(e) {
-                $('#blah').attr('src', e.target.result);
+    Window.onload= function (e) {
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                console.log('ditm me');
+                reader.onload = function(e) {
+                    $('#blah').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
             }
-            reader.readAsDataURL(input.files[0]);
         }
+        $("#av").change(function() {
+            readURL(this);
+        });
+
     }
-    $("#av").change(function() {
-        readURL(this);
-    });
     function checkRegister() {
-        var username = $('#username').html();
-        var email = $('#email').html();
-        var phone = $('#phone').html();
+        var username = $('#username').val();
+        var email = $('#email').val();
+        var phone = $('#phone').val();
+        var firstname = $('#firstname').val();
+        var lastname = $('#lastname').val();
+        var password = $('#password').val();
+        var confirmpassword = $('#confirmpassword').val();
         $.ajax({
-            url: '{{route('post.test')}}',
+            url: '{{route('post.Register')}}',
             type: 'POST',
+            dataType: "json",
             data: {
                 username: username,
                 email:email,
                 phone:phone,
-                _token: $('#register').val(),
+                firstname:firstname,
+                lastname:lastname,
+                password:password,
+                confirmpassword:confirmpassword,
+                _token: "{{csrf_token()}}",
             },
+            success: function (data) {
+                $('#list').append(data.view);
+            },
+            error :function( data ) {
+                if (data.status === 422) {
+                    var errors = $.parseJSON(data.responseText);
+                    $.each(errors, function (key, value) {
+
+                        $('#response').addClass("alert alert-danger");
+                        if ($.isPlainObject(value)) {
+                            $.each(value, function (key, value) {
+                                console.log(key + " " + value);
+                                $("#"+key).val("");
+                                $("#"+key).attr("placeholder", value);
+
+                            });
+                        } else {
+                            $("#"+key).val("");
+                            $("#"+key).attr("placeholder", value);//this is my div with messages
+                        }
+
+                    });
+                }
+            }
         })
     }
 </script>
